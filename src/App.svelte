e0b436b72639b210e88f36f84e124cac50912549
Kruistz <lekruistz@gmail.com>
2024-06-22 07:59:12 +0000

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -70,6 +70,13 @@
   <rect height="9" width="3" x="7" y="7" />
   <rect height="5" width="3" x="14" y="7" />
 </svg>Projects</a></li>
+<li>
+<a class="drawer-item">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="main-grid-item-icon w-5 h-5 fill-stroke mr-1.5 ml-2" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+  <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
+  <circle cx="12" cy="13" r="4" />
+</svg>
+Gallery</a></li>
     </ul>
   </div>
   </div>
