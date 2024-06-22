ccc38e7a18a5e69e408fe7e0b72a0be1f0abfba3
itskruistz <lekruistz@gmail.com>
2024-06-22 08:02:23 +0700

    Update

--- src/App.svelte
+++ src/App.svelte
@@ -78,10 +78,6 @@
     <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-left text-left">
       <h1 class="title-font text-4xl mb-4 font-figtree font-bold">Hello, I'm Kruistz!</h1>
       <p class="mb-8 leading-normal font-figtree text-lg">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
-      <div class="flex justify-center">
-        <button class="btn btn-primary font-figtree text-lg rounded-full">Button</button>
-        <button class="btn ml-2.5 font-figtree  text-lg rounded-full">Button</button>
-      </div>
     </div>
     <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
       <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600">
