1913b91760178c400f45190eed73ee2f6587c8db
Kruistz <lekruistz@gmail.com>
2024-06-24 14:17:54 +0000

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -41,7 +41,7 @@
 
   
 </label>
-<details class="dropdown">
+<details class="dropdown dropdown-end">
 <summary class="btn btn-square btn-ghost">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="main-grid-item-icon inline-block w-5 h-5 stroke-current" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
   <circle cx="12" cy="12" r="1" />
