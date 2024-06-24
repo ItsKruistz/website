f10ce599817c119714d1521ca3d4a78e6387f9ac
Kruistz <lekruistz@gmail.com>
2024-06-24 14:13:39 +0000

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -41,13 +41,19 @@
 
   
 </label>
-<button class="btn btn-square btn-ghost">
+<details class="dropdown">
+<summary class="btn btn-square btn-ghost">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="main-grid-item-icon inline-block w-5 h-5 stroke-current" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
   <circle cx="12" cy="12" r="1" />
   <circle cx="19" cy="12" r="1" />
   <circle cx="5" cy="12" r="1" />
 </svg>
-    </button>
+</summary>
+    <ul class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+    <li><a>Item 1</a></li>
+    <li><a>Item 2</a></li>
+  </ul>
+    </details>
   </div>
 </div>
   <div class="drawer">
